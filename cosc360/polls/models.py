c25f0a3ea071af050c8eb42f9fd45ceb1627ad90
Benjamin Leon <benleon@msn.com>
2017-11-20 16:42:41 -0800

    fixed some styling issues

--- cosc360/polls/models.py
+++ cosc360/polls/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -17,6 +18,7 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
